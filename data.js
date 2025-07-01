24a965842330ebcb46542c97f2586248d023ebe1
Timmydrax <timmydrax@gmail.com>
2025-07-01 23:39:55 +0100

    CHORE: Added dynamic rendering

---
+++ data.js
@@ -0,0 +1,44 @@
+export const blogArray = [
+  {
+    src: "blog-image-01.png",
+    date: "JULY 23, 2022",
+    heading: "Blog one",
+    text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+  },
+  {
+    src: "blog-image-02.png",
+    date: "JULY 23, 2022",
+    heading: "Blog two",
+    text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+  },
+  {
+    src: "blog-image-03.png",
+    date: "JULY 23, 2022",
+    heading: "Blog three",
+    text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+  },
+  {
+    src: "blog-image-04.png",
+    date: "JULY 23, 2022",
+    heading: "Blog four",
+    text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+  },
+  {
+    src: "blog-image-05.png",
+    date: "JULY 23, 2022",
+    heading: "Blog five",
+    text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+  },
+  {
+    src: "blog-image-06.png",
+    date: "JULY 23, 2022",
+    heading: "Blog six",
+    text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+  },
+];

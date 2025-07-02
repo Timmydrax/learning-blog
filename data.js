0abbd7899492f90a3e0234a746e5424557cb7b6f
Timmydrax <timmydrax@gmail.com>
2025-07-02 01:35:39 +0100

    STYLE: Styled Home-page

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 export const blogArray = [
   {
     src: "blog-image-01.png",
+    alt: "A picture of a laptop on the table",
     date: "JULY 23, 2022",
     heading: "Blog one",
     text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
@@ -8,6 +9,7 @@ export const blogArray = [
   },
   {
     src: "blog-image-02.png",
+    alt: "A hand holding a bulb",
     date: "JULY 23, 2022",
     heading: "Blog two",
     text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
@@ -15,6 +17,7 @@ export const blogArray = [
   },
   {
     src: "blog-image-03.png",
+    alt: "A purple and black 3-D image",
     date: "JULY 23, 2022",
     heading: "Blog three",
     text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
@@ -22,6 +25,7 @@ export const blogArray = [
   },
   {
     src: "blog-image-04.png",
+    alt: "A young lady enjoying the sunshine happily",
     date: "JULY 23, 2022",
     heading: "Blog four",
     text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
@@ -29,6 +33,7 @@ export const blogArray = [
   },
   {
     src: "blog-image-05.png",
+    alt: "An image showcasing the react logo on a laptop screen",
     date: "JULY 23, 2022",
     heading: "Blog five",
     text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
@@ -36,6 +41,7 @@ export const blogArray = [
   },
   {
     src: "blog-image-06.png",
+    alt: "A laptop on the table with a writing that says design and development",
     date: "JULY 23, 2022",
     heading: "Blog six",
     text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 import { blogArray } from "./data.js";
+
+const blog = document.getElementById("blog");
+
+function renderBlog(blogArray) {
+  const blogContent = blogArray
+    .map((blog, index) => {
+      const { src, date, heading, text, alt } = blog;
+
+      return `
+        <article class="card">
+        <img class="card-image" src="assets/images/${src}" alt="${alt}"/>
+        <p class="card-date">${date}</p>
+        <h2 class="card-heading">${heading}</h2>
+        <p class="card-text">${text} </p>
+        </article>  
+        `;
+    })
+    .join("");
+
+  return blogContent;
+}
+
+blog.innerHTML = renderBlog(blogArray);
